95e926bb93ecb35ffb2de1291e783d2e95713bac
MightyBumi <stitofte@1337.no>
2015-06-07 01:36:19 +0200

    Trying to prepare for the throw event and explosion....

--- src/no/bumi/events/Explosion.java
+++ src/no/bumi/events/Explosion.java
@@ -1,11 +1,24 @@
 package events;
 
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.entity.player.PlayerDropItemEvent;
+import org.spongepowered.api.event.entity.player.PlayerEvent;
+import org.spongepowered.api.text.Texts;
+import util.HashLists;
+
 /**
  * Created by Stian on 07.06.2015.
  */
 public class Explosion {
 
-    public void Explosion(){
+    @Subscribe
+    public void Explosion(PlayerDropItemEvent e){
         //This void will contain all the explosion code!
+        Player player = e.getUser();
+        String stringPlayer = e.getUser().getName().toString();
+        if (HashLists.checkMap(stringPlayer)){
+            player.sendMessage(Texts.of("You just dropped something??!"));
+        }
     }
 }
